6f91f3504861b16cf4462d76fca810fc6bbdc919
VladyslavGryban <ipz214_gsv1@student.ztu.edu.ua>
2022-03-30 12:23:36 +0300

    Added project to remote repository

---
+++ HelloNameApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using HelloNameApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+namespace HelloNameApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string HelloName)
+        {
+            ViewBag.Message = $"Hello, {HelloName}";
+            return View("PostIndex");
+        }
+    }
+}
+
